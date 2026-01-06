05403636bae48f8cc52c55059c8932571cb45810
yvan Jabez <111864110+yvan521@users.noreply.github.com>
2026-01-06 23:57:27 +0200

    Update server.js

--- Backend/src/server.js
+++ Backend/src/server.js
@@ -13,7 +13,14 @@ import reportRoutes from '../routes/reports.js';
 dotenv.config();
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true,
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"]
+}));
+
+app.options("*", cors());
 app.use(express.json());
 app.use(morgan('dev')); 
 
@@ -28,3 +35,7 @@ app.use('/api/reports', reportRoutes);
 
 const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => console.log(`Server listening on http://localhost:${PORT}`));
+
+
+
+
